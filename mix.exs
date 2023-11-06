afc14933db1785a1db79276544b7b2ad8d6b43bc
Susumu Yamazaki <zacky1972@gmail.com>
2023-11-06 21:18:33 +0900

    change elixir version of project

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SampleProject.MixProject do
     [
       app: :sample_project,
       version: "0.1.0",
-      elixir: "~> 1.16-rc",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
